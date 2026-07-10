47aa4b292fed7845e3a000f2145d7cef5aee4d42
Anil <anilkumar.sunkara@citrix.com>
2026-07-10 16:23:12 +0530

    .

--- conftest.py
+++ conftest.py
@@ -3,45 +3,10 @@ from selenium import webdriver
 
 @pytest.fixture
 def setup_and_teardown():
-    driver = webdriver.Chrome()   ## setup
+    driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get('https://demowebshop.tricentis.com/')
+    driver.get("https://demowebshop.tricentis.com/")
+
     yield driver
-    driver.quit()     ## teardown
-    driver.get('https://demowebshop.tricentis.com/')
-    yield driver
+
     driver.quit()
-
-
-## Cross browser Testing
-
-
-import pytest
-
-from selenium import webdriver
-
-@pytest.fixture(params=['chrome','edge','firefox'])
-def setup_and_teardown(request):
-    parameter = request.param   ## store the current parameter
-
-    if parameter == 'chrome':
-        driver = webdriver.Chrome()
-    elif parameter == 'edge':
-        driver = webdriver.Edge()
-    elif parameter == 'firefox':
-        driver = webdriver.Firefox()
-
-    driver.maximize_window()
-    driver.get('https://demowebshop.tricentis.com/')
-    yield driver
-    driver.quit()
-
-# # request is a built-in pytest object
-# # Whenever a fixture is parameterized , the current parameter is stored
-#  inside request.parm
-#
-# # First execution --> request.parm = 'chrome'
-# # Second execution --> request.parm = 'edge'....
-
-# #comment
-# >>>>>>> 917679db902af80ec6733bab4d7ad6a1d7b8e338
