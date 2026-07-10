da783dc60578ae1db0caa1eae348e1da3456ea93
Anil <anilkumar.sunkara@citrix.com>
2026-07-10 16:27:22 +0530

    .

--- conftest.py
+++ conftest.py
@@ -1,9 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def setup_and_teardown():
-    driver = webdriver.Chrome()
+
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+
     driver.maximize_window()
     driver.get("https://demowebshop.tricentis.com/")
 
